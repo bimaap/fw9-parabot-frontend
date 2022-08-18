b778f34aaebb5f05ecb395c80095536bc66c817f
bimaap <let.bimaap@gmail.com>
2022-08-18 11:26:49 +0700

    chore: setup tailwindcss

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header(){
+    return(
+        <header className="bg-zinc-300 w-full h-8 flex items-center justify-center">
+            Header
+        </header>
+    )
+}
