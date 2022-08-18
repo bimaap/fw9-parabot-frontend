4e40ddae173372796efa06e36e83d1423f75bf97
bimaap <let.bimaap@gmail.com>
2022-08-18 14:29:28 +0700

    feat: header&footer

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,30 @@
 
+import { TbSearch, TbHeart, TbShoppingCart, TbAlignRight, TbChevronDown } from "react-icons/tb";
+
 export default function Header(){
     return(
-        <header className="bg-zinc-300 w-full h-8 flex items-center justify-center">
-            Header
+        <header className="bg-slate-100 h-24 flex items-center justify-center">
+            <div className="w-[1200px] flex justify-between items-center">
+                <span className="text-3xl font-medium text-gray-700">ParaBot</span>
+                <div className="flex gap-10 text-gray-700">
+                    <span className="font-semibold">HOME</span>
+                    <div className="flex items-center gap-1">
+                        <span className="font-semibold">PAGES</span>
+                        <TbChevronDown />
+                    </div>
+                    <div className="flex items-center gap-1">
+                        <span className="font-semibold">SHOP</span>
+                        <TbChevronDown />
+                    </div>
+                    <span className="font-semibold">BLOG</span>
+                </div>
+                <div className="flex gap-8 text-center justify-center text-gray-700">
+                    <TbSearch className="text-2xl" />
+                    <TbHeart className="text-2xl" />
+                    <TbShoppingCart className="text-2xl" />
+                    <TbAlignRight className="text-2xl" />
+                </div>
+            </div>
         </header>
     )
 }
