286b19f08db6d09bbd11b302b2db7ca1038225ef
bimaap <let.bimaap@gmail.com>
2022-08-18 22:07:08 +0700

    feat: landing page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,41 +1,42 @@
 
 import { SiFacebook, SiTwitter, SiYoutube, SiDribbble } from 'react-icons/si';
 import { TbMail, TbPhone, TbClock } from 'react-icons/tb';
+import Link from 'next/link';
 
 export default function Footer(){
     return(
         <footer className="bg-zinc-600 flex justify-center items-center w-full">
-            <div className="flex flex-col w-[1400px] justify-between items-center text-slate-300">
+            <div className="flex flex-col max-w-[1400px] flex-1 justify-between items-center text-slate-300">
                 <div className="w-full py-20 flex justify-between">
                     <div className="flex flex-col w-80 gap-8">
-                        <span className="text-3xl font-medium">ParaBot</span>
+                        <Link href={'/'}><span className="text-3xl font-medium cursor-pointer">ParaBot</span></Link>
                         <span>Donec nunc nunc, gravida vitae diam vel, varius interdum erat. Quisque a nunc vel diam auctor commodo.</span>
                         <div className='flex gap-3'>
-                            <div className='p-2 rounded-full border-cyan-50 border-2'><SiFacebook /></div>
-                            <div className='p-2 rounded-full border-cyan-50 border-2'><SiTwitter /></div>
-                            <div className='p-2 rounded-full border-cyan-50 border-2'><SiYoutube /></div>
-                            <div className='p-2 rounded-full border-cyan-50 border-2'><SiDribbble /></div>
+                            <Link href={'/#'}><div className='p-2 rounded-full border-cyan-50 border-2 cursor-pointer'><SiFacebook /></div></Link>
+                            <Link href={'/#'}><div className='p-2 rounded-full border-cyan-50 border-2 cursor-pointer'><SiTwitter /></div></Link>
+                            <Link href={'/#'}><div className='p-2 rounded-full border-cyan-50 border-2 cursor-pointer'><SiYoutube /></div></Link>
+                            <Link href={'/#'}><div className='p-2 rounded-full border-cyan-50 border-2 cursor-pointer'><SiDribbble /></div></Link>
                         </div>
                     </div>
 
                     <div className='flex flex-col gap-8'>
                         <span className='text-md font-medium'>COMPANY</span>
                         <div className='flex flex-col gap-4'>
-                            <span className='text-sm'>About Us</span>
-                            <span className='text-sm'>Help Center</span>
-                            <span className='text-sm'>Licenses</span>
-                            <span className='text-sm'>Market API</span>
-                            <span className='text-sm'>Site Map</span>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>About Us</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Help Center</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Licenses</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Market API</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Site Map</span></Link>
                         </div>
                     </div>
 
                     <div className='flex flex-col gap-8'>
                         <span className='text-md font-medium'>USERFUL</span>
                         <div className='flex flex-col gap-4'>
-                            <span className='text-sm'>The Collections</span>
-                            <span className='text-sm'>Size Guide</span>
-                            <span className='text-sm'>Lookbook</span>
-                            <span className='text-sm'>Instagram Shop</span>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>The Collections</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Size Guide</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Lookbook</span></Link>
+                            <Link href={'/#'}><span className='text-sm cursor-pointer'>Instagram Shop</span></Link>
                         </div>
                     </div>
 
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,58 @@
 
+import React from 'react';
 import { TbSearch, TbHeart, TbShoppingCart, TbAlignRight, TbChevronDown } from 'react-icons/tb';
+import Link from 'next/link';
 
 export default function Header(){
+    const [page, setPage] = React.useState({active: false, left: 0, top: 0})
+    const [shop, setShop] = React.useState({active: false, left: 0, top: 0})
+
+    const pagePos = (e) => {
+        setPage({active: !page.active, left: e.pageX - 60, top: e.pageY + 30})
+    }
+
+    const pageShop = (e) => {
+        setShop({active: !shop.active, left: e.pageX - 60, top: e.pageY + 30})
+    }
     return(
         <header className="bg-slate-100 h-28 flex items-center justify-center">
-            <div className="w-[1400px] flex justify-between items-center">
-                <span className="text-3xl font-medium text-gray-700">ParaBot</span>
+            <div className="max-w-[1400px] flex-1 flex justify-between items-center">
+                <Link href={'/'}><span className="text-3xl font-medium cursor-pointer">ParaBot</span></Link>
                 <div className="flex gap-10 text-gray-700">
-                    <span className="font-semibold">HOME</span>
-                    <div className="flex items-center gap-1">
+                    <Link href={'/#'}><span className="font-semibold cursor-pointer">HOME</span></Link>
+                    <div className="flex items-center gap-1 cursor-pointer" id="page" onClick={(e)=> pagePos(e)}>
                         <span className="font-semibold">PAGES</span>
                         <TbChevronDown />
                     </div>
-                    <div className='flex items-center gap-1'>
+                    {
+                        page.active&&
+                        <div style={{top: page.top, left: page.left}} className={`absolute rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}>
+                            <div className="py-1" role="none">
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Account settings</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-1">Support</a>
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-2">License</a>
+                            </div>
+                        </div>
+                    }
+                    <div className='flex items-center gap-1 cursor-pointer' onClick={(e)=> pageShop(e)}>
                         <span className='font-semibold'>SHOP</span>
                         <TbChevronDown />
                     </div>
-                    <span className='font-semibold'>BLOG</span>
+                    {
+                        shop.active&&
+                        <div style={{top: shop.top, left: shop.left}} className={`absolute rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}>
+                            <div className="py-1" role="none">
+                                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Account settings</a>
+                            </div>
+                        </div>
+                    }
+                    <Link href={'/#'}><span className='font-semibold cursor-pointer'>BLOG</span></Link>
                 </div>
                 <div className='flex gap-8 text-center justify-center text-gray-700'>
-                    <TbSearch className='text-2xl' />
-                    <TbHeart className='text-2xl' />
-                    <TbShoppingCart className='text-2xl' />
-                    <TbAlignRight className='text-2xl' />
+                    <Link href={'/#'}><TbSearch className='text-2xl cursor-pointer' /></Link>
+                    <Link href={'/#'}><TbHeart className='text-2xl cursor-pointer' /></Link>
+                    <Link href={'/#'}><TbShoppingCart className='text-2xl cursor-pointer' /></Link>
+                    <Link href={'/#'}><TbAlignRight className='text-2xl cursor-pointer' /></Link>
                 </div>
             </div>
         </header>
