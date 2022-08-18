e2cb220908ad3f166a59def8232ac6ddee41953c
afrizalsofyan <afrizalafaandy@gmail.com>
2022-08-18 20:15:42 +0700

    fix: fix error warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
-// <<<<<<< HEAD
 import '../styles/globals.css';
 import '../styles/cartStyle.css';
-// =======
-// import '../styles/globals.css';
-// >>>>>>> ff2d38832ad0d70080d583a7da4e367964600d1a
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
