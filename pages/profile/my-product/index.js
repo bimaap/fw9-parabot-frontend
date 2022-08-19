4debd26b98f45fccb8e9e9f7278299f857699b57
afrizalsofyan <afrizalafaandy@gmail.com>
2022-08-19 14:22:20 +0700

    fix: update banner with my-product page

---
+++ pages/profile/my-product/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Banner from '../../../components/Banner';
+
+function MyProduct() {
+    return (
+        <Banner titleBanner='MyProduct' subtitleBanner='test'/>
+    );
+}
+
+export default MyProduct;
